927660d8d1d298ba2a6cbaeddfa36f84d03f2da6
jamo254 <70902444+jamo254@users.noreply.github.com>
2021-10-22 23:08:41 +0300

    Create the Questions and Choices models

--- apicore/models.py
+++ apicore/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+import datetime
+from django.utils import timezone
+# Модели для опоросов
+class Question(models.Model):
+    poll_question = models.CharField(max_length=255, blank=False)
+    title = models.CharField(max_length=255, blank=True)
+    start_date = models.DateTimeField('date published', blank=False)
+    end_date = models.DateTimeField('end date', blank=True)
+    
+    def __str__(self):
+        return self.poll_question
+    
+    def checking_publishing_date(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.start_date <= now
 
-# Create your models here.
+#Модель для Выбора
+class Choice(models.Model):
+    poll_question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    poll_question_choice = models.CharField(max_length=255)
+    votes = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.poll_question_choice
+    
+    
